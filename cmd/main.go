f4b7343b8b08b8fc41a6dcd559a4e534bbfab09d
Michael Valdes <michael.j.valdes@gmail.com>
2022-01-26 19:20:31 -0500

    Add simple README

--- cmd/main.go
+++ cmd/main.go
@@ -8,22 +8,6 @@ import (
 	"path/filepath"
 )
 
-/* TODO
-create structs for inputs
-accept terminal inputs
-accept config inputs
-validate inputs
-- reorganize code into packages
-- combine all functions into one main.go (this one)
-- create 4 maps
-- combine 4 maps into one html site
-- create temp files, destroy
-- deal with paths responsibly
-deal with path to unmined-cli in mapgen
-add args to choose between gen, deploy, both, and both at a frequency
-add helpful comments and handle errors
-*/
-
 func main() {
 	exePath, err := os.Executable()
 	if err != nil {
